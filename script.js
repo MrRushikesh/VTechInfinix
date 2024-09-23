fb7a063007771455ce184b8ad75fd126ec7379ae
MrRushikesh <rushikeshingale7447@gmail.com>
2024-09-23 21:21:12 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Website Designe & Developement","Mobile Apps Development","Software Development","Digital Marketing & Social Media Handling"],
+        strings: ["Website Design", "Mobile Applications", "Software Development", "Digital Marketing", "Graphics Designing", "Social Media Handling"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -51,20 +51,20 @@ $(document).ready(function(){
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
-        // loop: true,
-        // autoplay: true,
-        // autoplayTimeOut: 2000,
-        // autoplayHoverPause: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 100,
+        autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
@@ -73,6 +73,6 @@ $(document).ready(function(){
 });
 
 
-function alertMsg (){
-    alert("Message Sent Successfully")
-}
+// function alertMsg() {
+//     alert("Message Sent Successfully")
+// }
